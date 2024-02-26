5fb7465f4915c5c0ee140ef5b59d382894768c28
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2024-02-26 14:46:07 -0300

    Add files via upload

---
+++ JeffsCar/Estacionamento.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeffsCar
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void adicionarVeiculo()
+        {
+            Console.WriteLine("Informe a placa do veículo: ");
+            string placa = Console.ReadLine();
+            placa = placa.ToUpper();
+
+            if (veiculos.Any(x => x.Equals(placa.ToUpper())))
+            {
+                Console.WriteLine("Já existe um veículo com essa placa.");
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veículo cadastrado com sucesso.");
+            }
+        }
+
+        public void removerVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo:");
+            string placa = Console.ReadLine();
+            placa = placa.ToUpper();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Informe a quantidade de horas que o veículo permaneceu no pátio: ");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo de placa: {placa} foi removido e deve pagar {precoTotal}.");
+            }
+            else
+            {
+                Console.WriteLine("O veículo não está em nosso pátio.");
+            }
+        }
+
+        public void listarVeiculo()
+        {
+            if(veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são: ");
+                foreach(var v in veiculos)
+                {
+                    Console.WriteLine(v);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos no pátio.");
+            }
+        }
+    }
+}
---
+++ JeffsCar/Program.cs
@@ -0,0 +1,56 @@
+﻿//Declarando variáveis
+
+using JeffsCar;
+
+decimal precoInicial = 0;
+decimal precoHora = 0;
+
+//Iniciar sistema
+
+Console.WriteLine("Sejam bem vindos ao estacionamento do Jeff");
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine("Digite o preço Inicial: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora: ");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+//Estancia a classe estacionamento
+
+Estacionamento es = new Estacionamento(precoInicial, precoHora);
+
+
+//Loop
+
+bool menu = true;
+
+while (menu)
+{
+    Console.Clear();
+    Console.WriteLine("Escolha uma opção.");
+    Console.WriteLine("1 - Adicionar veículo.");
+    Console.WriteLine("2 - Remover veículo.");
+    Console.WriteLine("3 - Listar veículo.");
+    Console.WriteLine("4 - Sair do sistema.");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.adicionarVeiculo();
+            break;
+        case "2":
+            es.removerVeiculo();
+            break;
+        case "3":
+            es.listarVeiculo();
+            break;
+        case "4":
+            menu = false;
+            break;
+        default: Console.WriteLine("Digite uma opção válida!");
+            break;
+    }
+
+    Console.WriteLine("Você saiu do sistema.");
+}
